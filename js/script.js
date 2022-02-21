e219c7a8164a9d233c15fde4d1892fdbe2a788b4
Brad Whipp <contactbradly@gmail.com>
2022-02-21 06:37:38 -0800

    OMG it was just a typo!

--- js/script.js
+++ js/script.js
@@ -3,25 +3,25 @@ const username = "Bradliness";
 
 const gitUserInfo = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
-    const data = await userInfo.json();
+  const data = await userInfo.json();
     displayUserInfo(data);
 };
 
 gitUserInfo();
 
 
- const displayUSerInfo = function (data) {
+ const displayUserInfo = function (data) {
   const div = document.createElement("div");
   div.classList.add("user-info");
   div.innerHTML = `
   <figure>
-    <img alt="user avatar" src=${data.avatar_url} />
+    <img alt="user avatar" src=${"https://avatars.githubusercontent.com/u/59591487?v=4"} />
   </figure>
   <div>
     <p><strong>Name:</strong> ${data.name}</p>
     <p><strong>Bio:</strong> ${data.bio}</P>
     <p><strong>Location:</strong> ${data.location}</p>
-    <p><strong>Number of publis repos:</strong> ${data.public_repos}</p>
+    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div>
   `;
   overview.append(div);
